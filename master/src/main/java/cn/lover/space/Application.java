f6582b8ef7111d110987191e93106284358dbc71
huanglm <2788447660@qq.com>
2020-03-29 16:10:17 +0800

    搭建后端环境

---
+++ master/src/main/java/cn/lover/space/Application.java
@@ -0,0 +1,18 @@
+package cn.lover.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * date: 2020-03-29
+ * time: 16:06
+ * author: huanglm520
+ * see: https://github.com/huanglm520
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
