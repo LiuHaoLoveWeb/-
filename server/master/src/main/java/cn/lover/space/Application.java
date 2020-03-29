a58bd7858848e2b3e9e2788b184b0e24b6dd452c
huanglm <2788447660@qq.com>
2020-03-29 17:26:10 +0800

    启用健康检查

--- server/master/src/main/java/cn/lover/space/Application.java
+++ server/master/src/main/java/cn/lover/space/Application.java
@@ -2,6 +2,7 @@ package cn.lover.space;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * date: 2020-03-29
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * see: https://github.com/huanglm520
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
